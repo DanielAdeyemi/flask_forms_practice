f318c03a05c7ad0ca09805ca09c81005655d8511
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-17 15:03:57 -0700

    add post method into the code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def home():
   return render_template("index.html")
 
-@app.route("/item/new")
+@app.route("/item/new", methods=["GET", "POST"])
 def new_item():
+  if request.method == "POST":
+    return redirect(url_for("home"))
   return render_template("new_item.html")
