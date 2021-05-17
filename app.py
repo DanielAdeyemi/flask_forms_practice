4ca40d94c7e63fe20868335f0a12bfb807557829
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-17 16:04:16 -0700

    add open and close connection for db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
+import sqlite3
 
 app = Flask(__name__)
 
@@ -8,6 +9,20 @@ def home():
 
 @app.route("/item/new", methods=["GET", "POST"])
 def new_item():
+  conn = get_db()
+  c = conn.cursor()
   if request.method == "POST":
     return redirect(url_for("home"))
   return render_template("new_item.html")
+
+def get_db():
+  db = getattr(g, "_database", None)
+  if db is None:
+    g._database = sqlite3.connect("db/globomantics.db")
+  return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+  db = getattr(g, "_database", None)
+  if db is not None:
+    db.close()
