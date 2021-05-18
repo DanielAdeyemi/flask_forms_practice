e221e8802a7459e7bd5f01fee73ef682f4ea5cf8
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-18 11:31:24 -0700

    add simple validation

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, g, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, FloatField
 import sqlite3
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretkey"
 
 class NewItemForm(FlaskForm):
   title = StringField("Title")
-  price = StringField("Price")
+  price = FloatField("Price")
   description = TextAreaField("Description")  
-  category = SelectField("Category")
-  subcategory = SelectField("Subcategory")
+  category = SelectField("Category", coerce=int)
+  subcategory = SelectField("Subcategory", coerce=int)
   submit = SubmitField("Submit")
 
 @app.route("/")
@@ -55,11 +56,11 @@ def new_item():
   subcategories = c.fetchall()
   form.subcategory.choices = subcategories
 
-  if request.method == "POST":
+  if form.validate_on_submit():
     c.execute("""INSERT INTO items(title, description, price, image, category_id, subcategory_id) VALUES(?,?,?,?,?,?)""", 
               (form.title.data,
               form.description.data,
-              float(form.price.data),
+              form.price.data,
               "",
               form.category.data,
               form.subcategory.data
@@ -67,6 +68,9 @@ def new_item():
     conn.commit()
     flash("Item {} has been successfully created".format(request.form.get("title")), "success")
     return redirect(url_for("home"))
+  
+  if form.errors:
+    flash(" {} ".format(form.errors), "danger")
   return render_template("new_item.html", form=form)
 
 def get_db():
